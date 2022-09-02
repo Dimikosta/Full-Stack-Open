4e491cdf564fb66638d9294e67d04327f2f27bef
Dimitri <dimitrikosta@gmail.com>
2022-09-02 04:05:01 -0400

    2.14 Waiting for API Activation

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,16 +3,20 @@ import axios from 'axios';
 
 const App = (props) => {
 const [countries, setCountries] = useState([])
+const [weather, setWeather] = useState([])
 const [search, setSearch] = useState('')
 
+//API Key for weather service
+const api_key = process.env.REACT_APP_API_KEY
 
-useEffect(() => {
-  axios
-    .get('https://restcountries.com/v3.1/all')
-    .then(response => {
-      setCountries(response.data)
-    })
-}, [])
+// Gets Country data using API
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
 
 const handleSearch = (event) => {
   setSearch(event.target.value)
@@ -38,7 +42,13 @@ const DisplayResults = ({ countries }) => {
   } else if (result.length === 1) {
       return <DisplayCountry country={result[0]} />
   } else if (result.length > 1 && result.length <=10) {
-    return result.map((country, index) => <div key={index}>{country.name.common}</div>)
+    return result.map((country, index) => {
+    return (
+        <div key={index}>{country.name.common}
+        <button type = "submit" onClick = {() => setSearch(country.name.common) } >Show</button >
+        </div>
+    )
+    })
   }
 }
 
@@ -52,6 +62,10 @@ const DisplayCountry = ({ country }) => {
       <h2>Languages:</h2>
       <DisplayLanguages languages={country.languages} />
       <img src={country.flags.png} alt="Country's Flag" />
+      <h2>Weather in {country.capital}</h2>
+      <p>latitude: {country.capitalInfo.latlng[0]}</p>
+      <p>longitude: {country.capitalInfo.latlng[1]}</p>
+      <DisplayWeather lat={country.capitalInfo.latlng[0]} lng={country.capitalInfo.latlng[1]} />
     </>
   )
 }
@@ -66,6 +80,28 @@ const DisplayLanguages = ({languages}) => {
     {list.map((lang, index) => <li key={index}>{lang}</li>)}
     </ul>
   )
+}
+
+const DisplayWeather = ({lat, lng}) => {
+// Gets Weather data
+useEffect(() => {
+  axios
+    .get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lng}&exclude=minutely,hourly,daily,alerts&appid=${api_key}`)
+    .then(response => {
+      setWeather(response.data)
+    })
+})
+
+  // return (
+  //   <div>
+  //     <p>Temperature: {weather.current.temp}</p>
+  //     <img src={`http://openweathermap.org/img/wn/${weather.current.weather.icon}@2x.png`} alt="Weather Icon" />
+  //     <p>Wind: {weather.current.wind_speed}</p>
+  //   </div>
+  // )
+
+  console.log(weather)
+  return <p>test</p>
 }
 
   return (
