85f49136142d0ccbe9110d764e6bf9ee8c1c05fd
Dimitri <dimitrikosta@gmail.com>
2022-09-02 04:06:08 -0400

    2.14

---
+++ part2/countries/src/components/SearchFilter.js
@@ -0,0 +1,15 @@
+const SearchFilter = ({ props }) => {
+
+    console.log(props)
+//     const filtered = props.countries.filter((country, index) => {
+//         let searchLC = props.search.toLowerCase()
+//         let countryLC = country.name.common.toLowerCase()
+//         return countryLC.includes(searchLC)
+//     })
+//     return filtered
+// }
+    return true
+
+}
+
+export default SearchFilter
